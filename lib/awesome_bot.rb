1e5d53d2101a2ee9706d5ff08a616551206a5e31
dkhamsing <dkhamsing8@gmail.com>
2015-12-09 19:25:46 -0800

    isolate cli

--- lib/awesome_bot.rb
+++ lib/awesome_bot.rb
@@ -1,3 +1,4 @@
+require 'awesome_bot/cli'
 require 'awesome_bot/command'
 
 # Validate awesome project URLs
--- lib/awesome_bot/command.rb
+++ lib/awesome_bot/command.rb
@@ -1,62 +1,15 @@
 require 'awesome_bot/links'
 require 'awesome_bot/statuses'
-require 'awesome_bot/version'
 require 'awesome_bot/white_list'
 
-# Command
+# Check links
 module AwesomeBot
   NUMBER_OF_THREADS = 10
-
-  OPTION_DUPE = 'allow-dupe'
-  OPTION_WHITE_LIST = 'white-list'
 
   STATUS_OK = '✓'
   STATUS_OTHER = 'x'
 
-  USAGE = "\t"
-
   class << self
-    def cli
-      option_d = "--#{OPTION_DUPE}"
-      option_w = "--#{OPTION_WHITE_LIST}"
-
-      if ARGV.count == 0
-        puts "Usage: #{PROJECT} <file> [#{option_d}] "\
-             "[#{option_w} item1,item2,..]\n"\
-             "#{USAGE} file \t\t Path to file \n"\
-             "#{USAGE} #{option_d} \t Skip checking for duplicate URLs \n"\
-             "#{USAGE} #{option_w} \t Comma separated URLs to white list \n"\
-             "\nVersion #{VERSION}, see #{PROJECT_URL} for more information"
-
-        exit
-      end
-
-      filename = ARGV[0]
-      puts "> Checking links in #{filename}"
-
-      if ARGV.count > 1
-        options = ARGV.drop 1
-
-        skip_dupe = options.include? option_d
-
-        if options.include? option_w
-          i = options.find_index(option_w) + 1
-          white_listed = options[i].split ','
-        end
-      end
-
-      begin
-        content = File.read filename
-      rescue => error
-        puts "File open error: #{error}"
-        exit 1
-      end
-
-      exit 1 unless check(content, white_listed, skip_dupe, true) == true
-
-      exit
-    end
-
     def check(content, white_listed = nil, skip_dupe = false, verbose = false)
       dupe_success = skip_dupe
 
