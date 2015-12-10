0fb251c780b68d20fa45c0e612f73d2ecf63b4dc
dkhamsing <dkhamsing@cirruspath.com>
2015-12-10 11:52:00 -0800

    add head option when getting statuses

--- lib/awesome_bot/statuses.rb
+++ lib/awesome_bot/statuses.rb
@@ -4,14 +4,23 @@ module AwesomeBot
   require 'parallel'
 
   class << self
-    def net_status(url)
+    def net_head_status(url)
+      Faraday.head(url).status
+    end
+
+    def net_get_status(url)
       Faraday.get(url).status
     end
 
-    def statuses(links, threads)
+    def statuses(links, threads, head = false)
       statuses = []
       Parallel.each(links, in_threads: threads) do |u|
-        status = net_status u
+        status =
+          if head
+            net_head_status u
+          else
+            net_get_status u
+          end
 
         yield status, u
 
