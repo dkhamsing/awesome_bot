959f3c9132dcc5cf32f3cf46126117ed0bfeabe2
dkhamsing <dkhamsing@cirruspath.com>
2016-02-01 15:38:30 -0800

    [cli] invalid options error

--- lib/awesome_bot/cli.rb
+++ lib/awesome_bot/cli.rb
@@ -66,6 +66,15 @@ module AwesomeBot
         exit 1
       end
 
+      # Check options
+      user_options = ARGV.select { |o| o.include? '--' }
+      options_diff = user_options - options
+      if options_diff.count > 0
+        puts "Error, invalid options: #{options_diff.join ', '} \n"
+        puts "Valid options are #{options.join ', '}"
+        exit 1
+      end
+
       begin
         content = File.read filename
       rescue => error
