6bd1d98bb4f620068ea72c9d1a968ab7fa567289
Kishan Bagaria <KishanBagaria@users.noreply.github.com>
2016-04-15 22:21:03 +0530

    Improved link finding

--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -4,6 +4,7 @@ describe AwesomeBot do
   describe "check" do
     context "given adoc link" do
       content = 'https://www.linkedin.com/topic/group/kotlin-developers?gid=7417237[LinkedIn: Kotlin Developers (Join!)^]'
+      content = GitHub::Markup.render('README.adoc', content)
       list = AwesomeBot::links_find content
       f = AwesomeBot::links_filter list
       value = f[0]
@@ -17,11 +18,12 @@ describe AwesomeBot do
       content = "[Eddystone](https://en.wikipedia.org/wiki/Eddystone_(Google))"\
       "her [dockerfiles](https://github.com/jfrazelle/dockerfiles))"\
       "# ng-flow [![Build Status](https://travis-ci.org/flowjs/ng-flow.svg)](https://travis-ci.org/flowjs/ng-flow) [![Coverage Status](https://coveralls.io/repos/flowjs/ng-flow/badge.svg?branch=master&service=github)](https://coveralls.io/github/flowjs/ng-flow?branch=master)      "
+      content = GitHub::Markup.render('README.md', content)
       list = AwesomeBot::links_find content
       f = AwesomeBot::links_filter list
-      expected = 5
+      expected = 6
       value = f.count
-      it "has 5 links" do
+      it "has 6 links" do
         expect(value).to eql(expected)
       end
     end
