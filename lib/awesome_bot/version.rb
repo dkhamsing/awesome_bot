3b05e933497e5ef3925c18887f9e9af707c556a2
dkhamsing <dkhamsing@cirruspath.com>
2016-05-04 08:31:04 -0700

    bump version to 1.8.4

--- lib/awesome_bot/version.rb
+++ lib/awesome_bot/version.rb
@@ -5,5 +5,5 @@ module AwesomeBot
                         'Great for "awesome" projects.'
   PROJECT_URL = 'https://github.com/dkhamsing/awesome_bot'
 
-  VERSION = '1.8.3'
+  VERSION = '1.8.4'
 end
