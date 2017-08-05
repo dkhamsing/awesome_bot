6da18cc8983bd76612ffe3cfc0cc10b2f7632cd0
dkhamsing <dkhamsing8@gmail.com>
2017-08-05 10:00:16 -0700

    Isolate output filtered

--- lib/awesome_bot/output.rb
+++ lib/awesome_bot/output.rb
@@ -75,6 +75,81 @@ module AwesomeBot
       [o, hash]
     end
 
+    def output_filtered(content, r, options, markdown, skip_dupe)
+
+      filtered_issues = []
+
+      puts "\nIssues :-("
+
+      unless markdown.nil?
+        print "> Markdown Validation \n"
+        if r.success_validate
+          puts "  OK #{STATUS_OK}"
+        else
+          r.validate.each_with_index do |x, i|
+            locv = loc(x, content)
+            error = RESULT_ERROR_MARKDOWN
+
+            hash = {
+              'loc'=> locv,
+              'link'=> x,
+              'error'=> error
+            }
+            filtered_issues.push hash
+
+            print "  #{i+1} "
+            print loc_formatted locv
+            puts " #{x} \n"
+          end
+        end
+      end
+
+      print "> Links \n"
+      if r.success_links(options)
+        puts "  OK #{STATUS_OK}"
+      else
+        o = order_by_loc r.statuses_issues(options), content
+        o.each_with_index do |x, k|
+          temp, h = output(x, k, pad_list(o), digits)
+          filtered_issues.push h
+          puts temp
+        end
+      end
+
+      unless skip_dupe
+        print "> Dupes \n"
+        if r.success_dupe
+          puts "  None #{STATUS_OK}"
+        else
+          dupe_hash = r.dupes.uniq.map do |x|
+            temp = {}
+            temp['url'] = x
+            temp
+          end
+          o = order_by_loc dupe_hash, content
+          largest = o.last['loc'].to_s.size
+          o.each_with_index do |d, index|
+            loc = d['loc']
+            url = d['url']
+            error = RESULT_ERROR_DUPE
+
+            hash = {
+              'loc'=> loc,
+              'link'=> url,
+              'error'=> error
+            }
+            filtered_issues.push hash
+
+            print "  #{pad_text index + 1, pad_list(r.dupes.uniq)}. "
+            print loc_formatted loc, largest
+            puts " #{url}"
+          end
+        end
+      end
+
+      filtered_issues
+    end    
+
     def output_redirect(x)
       if status_is_redirected? x['status']
         " #{STATUS_REDIRECT} #{x['headers']['location']}"
