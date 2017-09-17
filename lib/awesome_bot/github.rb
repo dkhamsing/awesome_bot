03de0e99317cae63f537a51d175f66535d564879
Moshe Immerman <moshe@egis-software.com>
2017-09-17 13:26:21 +0200

    fail fast with empty GH_TOKEN

--- lib/awesome_bot/github.rb
+++ lib/awesome_bot/github.rb
@@ -10,6 +10,9 @@ module AwesomeBot
 
       user = ENV["GH_USER"]
       token = ENV["GH_TOKEN"]
+      if token.nil? or token == ""
+        abort ("Must specify a GH_TOKEN env variable")
+      end
       repo = URI.parse(url)
       path = repo.path.split("/").select do |i| i != '' end
       return unless repo.hostname == 'github.com' and path.length == 2
