acd1cf26b6675dd09449c4b25f7f0485d7e09aee
Moshe Immerman <moshe@egis-software.com>
2017-09-17 13:48:46 +0200

    make GH_TOKEN optional

--- lib/awesome_bot/github.rb
+++ lib/awesome_bot/github.rb
@@ -10,14 +10,22 @@ module AwesomeBot
 
       user = ENV["GH_USER"]
       token = ENV["GH_TOKEN"]
-      if token.nil? or token == ""
-        abort ("Must specify a GH_TOKEN env variable")
-      end
       repo = URI.parse(url)
       path = repo.path.split("/").select do |i| i != '' end
       return unless repo.hostname == 'github.com' and path.length == 2
-      json = HTTP.basic_auth(:user => user, :pass => token).get("https://api.github.com/repos/#{path[0]}/#{path[1]}/commits").to_s
-      (DateTime.now() - DateTime.parse(JSON.parse(json)[0]["commit"]["author"]["date"])).to_i
+      uri = "https://api.github.com/repos/#{path[0]}/#{path[1]}/commits"
+      if token.nil? or token == ""
+          json = HTTP.get(uri).to_s
+      else
+          json = HTTP.basic_auth(:user => user, :pass => token).get(uri).to_s
+      end
+
+      json = JSON.parse(json)
+      if @json..kind_of?(Hash) and not json["message"].nil?
+        puts json["message"]
+        return -1;
+      end
+      (DateTime.now() - DateTime.parse(json[0]["commit"]["author"]["date"])).to_i
     end
   end
 end
