207355aee7b895ba3b2a83f451afa1b3f0ab0eb5
Andy Ortlieb <andyortlieb@gmail.com>
2019-06-14 11:29:32 -0500

    Version bump and alternate docker build

--- lib/awesome_bot/version.rb
+++ lib/awesome_bot/version.rb
@@ -5,5 +5,5 @@ module AwesomeBot
                         'Great for "awesome" projects.'
   PROJECT_URL = 'https://github.com/dkhamsing/awesome_bot'
 
-  VERSION = '1.18.0'
+  VERSION = '1.18.1'
 end
