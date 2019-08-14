20825e916071cc8ddf44c56dd56d2102c183c623
dkhamsing <dkhamsing@users.noreply.github.com>
2019-08-14 15:41:07 -0700

    Update timeout spec

--- spec/timeout_spec.rb
+++ spec/timeout_spec.rb
@@ -8,7 +8,7 @@ describe AwesomeBot do
     context "given a timeout link and setting timeout to 1s" do
       r = AwesomeBot::check timeoutlink, options
       s = r.statuses_issues[0]
-      expected = 'execution expired'
+      expected = 'timed out'
       value = s['error'].to_s
       it "has a timeout" do
         expect(value).to include(expected)
